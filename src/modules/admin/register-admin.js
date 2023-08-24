ec5a3aa0dd368c8dc872ace2af499809068337b4
Baxtiyorov She'rozbek <91471554+sherozbek1706@users.noreply.github.com>
2023-08-24 20:23:28 +0500

    usernameni tekshirib oldim oldin shu username orqali hech kim kirmaganmi deb.

--- src/modules/admin/register-admin.js
+++ src/modules/admin/register-admin.js
@@ -2,6 +2,13 @@ const Admin = require("./Admin");
 
 const registerAdmin = async ({ body }) => {
   const admin = await Admin.create(body);
+  const { username, password, ...data } = body;
+
+  const existed = await Admin.findOne({ username });
+
+  if (existed) {
+    throw new BadRequestError("This username already existed!");
+  }
   return admin;
 };
 module.exports = registerAdmin;
