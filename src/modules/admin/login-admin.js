e81ba9df42f61f92bb7e7a2a5000299d2e7333c8
Baxtiyorov She'rozbek <91471554+sherozbek1706@users.noreply.github.com>
2023-08-24 20:19:06 +0500

    hamma amal to'g'ri bo'lsa, token generatsiya qilinishi kerak.

--- src/modules/admin/login-admin.js
+++ src/modules/admin/login-admin.js
@@ -1,6 +1,9 @@
 const Admin = require("./Admin");
 const { NotFoundError, ForbiddenError } = require("../../shared/errors");
 const { compareSync } = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const config = require("../../shared/config");
+
 const loginAdmin = async ({ body }) => {
   const { username, password } = body;
 
@@ -16,5 +19,11 @@ const loginAdmin = async ({ body }) => {
     throw new ForbiddenError("Password Incorrect!");
   }
 
+  const token = jwt.sign({ admin: { id: existing.id } }, config.jwt.secret, {
+    expiresIn: config.jwt.expirec_in,
+  });
+
+  return token;
 };
+
 module.exports = loginAdmin;
