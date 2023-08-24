31e6fdb01aa4b4d8f2b8119a810f81222a566f9a
Baxtiyorov She'rozbek <91471554+sherozbek1706@users.noreply.github.com>
2023-08-24 16:50:59 +0500

    BadRequestErrorni yasab oldim class orqali.

---
+++ src/shared/errors/index.js
@@ -0,0 +1,10 @@
+class BadRequestError extends Error {
+  constructor(msg) {
+    super(msg);
+
+    if (Error.captureStackTrace) {
+      Error.captureStackTrace(this, BadRequestError);
+    }
+  }
+}
+
