7a6f7dbcdd34c8ccd2bc688635ed7803000a7951
Baxtiyorov She'rozbek <91471554+sherozbek1706@users.noreply.github.com>
2023-08-24 20:14:13 +0500

    username bormi shuni tekshirdim

--- src/modules/admin/login-admin.js
+++ src/modules/admin/login-admin.js
@@ -1,4 +1,13 @@
 const Admin = require("./Admin");
+const { NotFoundError, ForbiddenError } = require("../../shared/errors");
 const loginAdmin = async ({ body }) => {
+  const { username, password } = body;
+
+  const existing = await Admin.findOne({ username });
+
+  if (!existing) {
+    throw new NotFoundError("Admin Not Found");
+  }
+
 };
 module.exports = loginAdmin;
