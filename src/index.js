03772dbe77fee20eafc922679ccec69814b26a7b
Ayubxon200811 <ayubxonrahimov20080811@gmail.com>
2023-08-24 14:30:09 +0500

    admin router indexga ulandi

--- src/index.js
+++ src/index.js
@@ -2,10 +2,17 @@ const express = require("express");
 const config = require("./shared/config");
 const db = require("./db");
 const app = express();
+// middleware 
+app.use(express.json())
+// import router
+const adminRoute = require("./modules/admin/_api");
 
-// DATABAZAGA ULANISH 
-db()
+// middleware router
+app.use(adminRoute);
 
-app.listen(config.port,() =>{
-    console.log(`SERVER HAS BEEN STARTED ON PORT:${config.port}`)
-})
+// DATABAZAGA ULANISH
+db();
+
+app.listen(config.port, () => {
+  console.log(`SERVER HAS BEEN STARTED ON PORT:${config.port}`);
+});
