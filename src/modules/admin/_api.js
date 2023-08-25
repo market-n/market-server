b6639a937d96a2bd5d305556e198d905cd820d4b
Baxtiyorov She'rozbek <91471554+sherozbek1706@users.noreply.github.com>
2023-08-25 15:22:09 +0500

    routerlarni import qildim

--- src/modules/admin/_api.js
+++ src/modules/admin/_api.js
@@ -1,8 +1,9 @@
 const express = require("express");
-const { register_admin } = require("./_controller");
+const { register_admin, login_admin } = require("./_controller");
 
 const router = express.Router();
 
 router.post("/register/admin", register_admin);
+router.post("/login/admin", login_admin);
 
 module.exports = router;
