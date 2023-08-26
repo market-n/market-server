4a626bcced945aa31c45143ddb83007f58f89c33
Baxtiyorov She'rozbek <91471554+sherozbek1706@users.noreply.github.com>
2023-08-26 17:16:28 +0500

    delete_admin o'zgardi -> remove_adminga

--- src/modules/admin/remove-admin.js
+++ src/modules/admin/remove-admin.js
@@ -2,19 +2,19 @@ const Admin = require("./Admin");
 
 const { NotFoundError } = require("../../shared/errors");
 
-const deleteAdmin = async ({ params }) => {
+const removeAdmin = async ({ params }) => {
   const existing = await Admin.findOne({ _id: params.id, is_deleted: false });
 
   if (!existing) {
     throw new NotFoundError("Admin Not Found!");
   }
 
-  const deletedAdmin = await Admin.findByIdAndUpdate(
+  const removedAdmin = await Admin.findByIdAndUpdate(
     { _id: params.id },
     { is_deleted: true },
     { new: true }
   );
 
-  return deletedAdmin;
+  return removedAdmin;
 };
-module.exports = deleteAdmin;
+module.exports = removeAdmin;
