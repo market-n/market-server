f8cdc5ff3a710cdb0d31226c9686c7dbfb73e6f5
Baxtiyorov She'rozbek <91471554+sherozbek1706@users.noreply.github.com>
2023-08-26 18:37:42 +0500

    hasRole yani Rollarni tekshirib olish uchun funksiya ochdim

---
+++ src/shared/auth/hasRole.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const { ForbiddenError } = require("../errors");
+
+/**
+ *
+ * @param {express.Request} req
+ * @param {express.Response} res
+ * @param {express.NextFunction} next
+ */
+
+const hasRole = (roles) => {
+};
+
+module.exports = hasRole;
