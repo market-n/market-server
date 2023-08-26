16176fc9e6be1c267a9fcf51f47fee50ed373cda
Baxtiyorov She'rozbek <91471554+sherozbek1706@users.noreply.github.com>
2023-08-26 18:46:01 +0500

    roleni tekshirib oldim.

--- src/shared/auth/hasRole.js
+++ src/shared/auth/hasRole.js
@@ -11,6 +11,12 @@ const { ForbiddenError } = require("../errors");
 const hasRole = (roles) => {
   return async (req, res, next) => {
     try {
+      const { role } = req.admin;
+
+      if (!roles.includes(role)) {
+        throw new ForbiddenError("This admin is not allowed this right!");
+      }
+
       next();
     } catch (error) {
       next(error);
