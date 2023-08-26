1c7cd0841ef29cef70bd3586ecbefcdb42c936a9
Baxtiyorov She'rozbek <91471554+sherozbek1706@users.noreply.github.com>
2023-08-26 17:40:02 +0500

    agar o'chirilgan admin bo'lmasa admin topilmadi deydi

--- src/modules/admin/unremove-admin.js
+++ src/modules/admin/unremove-admin.js
@@ -3,5 +3,11 @@ const Admin = require("./Admin");
 const { NotFoundError } = require("../../shared/errors");
 
 const unremoveAdmin = async ({ params }) => {
+  const existing = await Admin.findOne({ _id: params.id, is_deleted: true });
+
+  if (!existing) {
+    throw new NotFoundError("Removed admin Not Found!");
+  }
+
 };
 module.exports = unremoveAdmin;
