2f0a988d311a8f213731f8e527c4804a1fad009d
Baxtiyorov She'rozbek <91471554+sherozbek1706@users.noreply.github.com>
2023-08-26 16:57:09 +0500

    Agar Admin bo'lmasa Topilmadi degan xato chiqsin!

--- src/modules/admin/remove-admin.js
+++ src/modules/admin/remove-admin.js
@@ -3,5 +3,11 @@ const Admin = require("./Admin");
 const { NotFoundError } = require("../../shared/errors");
 
 const deleteAdmin = async ({ params }) => {
+  const existing = await Admin.findOne({ _id: params.id, is_deleted: false });
+
+  if (!existing) {
+    throw new NotFoundError("Admin Not Found!");
+  }
+
 };
 module.exports = deleteAdmin;
