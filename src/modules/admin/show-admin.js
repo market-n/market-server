1a7e71be06cec4b4e3f248fb9e53b6c9034671bc
Baxtiyorov She'rozbek <91471554+sherozbek1706@users.noreply.github.com>
2023-08-26 16:28:10 +0500

    Tekshirib olindi rostdan ham bormi degan id orqali

--- src/modules/admin/show-admin.js
+++ src/modules/admin/show-admin.js
@@ -5,6 +5,9 @@ const { NotFoundError } = require("../../shared/errors");
 const showAdmin = async ({ params }) => {
   const existing = await Admin.findOne({ _id: params.id, is_deleted: false });
 
+  if (!existing) {
+    throw new NotFoundError("Admin Not Found!");
+  }
 
   return existing;
 };
