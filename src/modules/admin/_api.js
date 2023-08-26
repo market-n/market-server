ea15e3b0bec8097c1a5699f2365ee5dd0b4c3d0b
Baxtiyorov She'rozbek <91471554+sherozbek1706@users.noreply.github.com>
2023-08-26 11:30:39 +0500

    Middlewareni chaqirib , bir o'zgaruvchiga saqlab oldim.

--- src/modules/admin/_api.js
+++ src/modules/admin/_api.js
@@ -1,9 +1,13 @@
 const express = require("express");
 const { register_admin, login_admin } = require("./_controller");
+const { isloggedIn } = require("../../shared/auth");
 
 const router = express.Router();
 
 router.post("/register/admin", register_admin);
+// Middlewarelarni bitta arrayga saqlab olib berib yuboramiz.
+const mRegisterAdmin = [isloggedIn];
+
 router.post("/login/admin", login_admin);
 
 module.exports = router;
