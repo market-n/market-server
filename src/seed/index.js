c4c45105c0f9e01f4c5e454bfe6bbdd141db6245
Baxtiyorov She'rozbek <91471554+sherozbek1706@users.noreply.github.com>
2023-08-27 20:14:52 +0500

    o'zgarmas o'zgaruvchlarni olish

--- src/seed/index.js
+++ src/seed/index.js
@@ -1,3 +1,5 @@
+const mongoose = require("mongoose");
+const config = require("../shared/config");
 
 // Collectionslarni import qilib olish.
 // Bu joyda yozganimni sababi ko'p Collections ishlatilishi mumkin!
