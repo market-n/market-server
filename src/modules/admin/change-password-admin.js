c3ab71c98c5afbd5ecd2698778263b8047365140
Baxtiyorov She'rozbek <91471554+sherozbek1706@users.noreply.github.com>
2023-08-27 16:48:15 +0500

    agar admin topilmasa xato bersi...

--- src/modules/admin/change-password-admin.js
+++ src/modules/admin/change-password-admin.js
@@ -9,6 +9,12 @@ const jwt = require("jsonwebtoken");
 const config = require("../../shared/config");
 
 const changePasswordAdmin = async ({ body, admin }) => {
+  const { current_password, new_password } = body;
+  const findAdmin = await Admin.findOne({ _id: admin.id, is_deleted: false });
+
+  if (!findAdmin) {
+    throw new NotFoundError("Admin Not Found");
+  }
   return { msg: "Your password was successfull changed!" };
 };
 
