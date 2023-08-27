338b4290ce2d847efe21f5503df0ad5d9586777b
Baxtiyorov She'rozbek <91471554+sherozbek1706@users.noreply.github.com>
2023-08-27 20:15:05 +0500

    mongo url oldim.

--- src/seed/index.js
+++ src/seed/index.js
@@ -5,3 +5,4 @@ const config = require("../shared/config");
 // Bu joyda yozganimni sababi ko'p Collections ishlatilishi mumkin!
 const User = require("../modules/users/Users");
 
+  const uri = `mongodb://${config.db.host}:${config.db.port}/${config.db.name}`;
