661096d3e0a7a5f05ea14085d823625c559788ff
Baxtiyorov She'rozbek <91471554+sherozbek1706@users.noreply.github.com>
2023-08-27 20:15:19 +0500

    databasega ulab oldim

--- src/seed/index.js
+++ src/seed/index.js
@@ -6,3 +6,16 @@ const config = require("../shared/config");
 const User = require("../modules/users/Users");
 
   const uri = `mongodb://${config.db.host}:${config.db.port}/${config.db.name}`;
+  mongoose.set("strictQuery", false);
+  mongoose
+    .connect(uri, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
+    .then(() => {
+      console.log("Connected to db");
+    })
+    .catch((err) => {
+      console.log("error", err);
+    });
+
