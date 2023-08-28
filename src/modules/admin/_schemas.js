63464e8cac7186f96acd58af055b1cc590343cb1
Baxtiyorov She'rozbek <91471554+sherozbek1706@users.noreply.github.com>
2023-08-28 12:24:17 +0500

    postaddadminschema yasab oldim

--- src/modules/admin/_schemas.js
+++ src/modules/admin/_schemas.js
@@ -1,2 +1,35 @@
 const Joi = require("joi");
 
+const postAddAdminSchemas = {
+  body: Joi.object({
+    first_name: Joi.string().min(4).max(64).required().messages({
+      "string.empty": "Firstname kiritilishi kerak!",
+      "string.min": "Firstname uzunligi 4 tadan ko'p bolishi kerak!",
+      "string.max": "Firstname uzunligi 64 tadan kam bolishi kerak!",
+      "any.required": "Firstname kiritilishi kerak!",
+    }),
+    last_name: Joi.string().min(4).max(64).required().messages({
+      "string.empty": "Lastname kiritilishi kerak!",
+      "string.min": "Lastname uzunligi 4 tadan ko'p bolishi kerak!",
+      "string.max": "Lastname uzunligi 64 tadan kam bolishi kerak!",
+      "any.required": "Lastname kiritilishi kerak!",
+    }),
+    image: Joi.string().required().messages({
+      "string.empty": "Image kiritilishi kerak!",
+      "any.required": "Image kiritilishi kerak!",
+    }),
+    username: Joi.string().min(6).max(64).required().messages({
+      "string.empty": "Username kiritilishi kerak!",
+      "string.min": "Username uzunligi 6 tadan ko'p bolishi kerak!",
+      "string.max": "Username uzunligi 64 tadan kam bolishi kerak!",
+      "any.required": "Username kiritilishi kerak!",
+    }),
+    password: Joi.string().min(6).max(64).required().messages({
+      "string.empty": "Password kiritilishi kerak!",
+      "string.min": "Password uzunligi 6 tadan ko'p bolishi kerak!",
+      "string.max": "Password uzunligi 64 tadan kam bolishi kerak!",
+      "any.required": "Password kiritilishi kerak!",
+    }),
+  }),
+};
+
