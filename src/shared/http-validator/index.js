11e4c6b36a29390654da0758a31603f43bf4c319
Baxtiyorov She'rozbek <91471554+sherozbek1706@users.noreply.github.com>
2023-08-28 12:21:59 +0500

    http validator ochdim

---
+++ src/shared/http-validator/index.js
@@ -0,0 +1,13 @@
+const Joi = require("joi");
+const { BadRequestError } = require("../errors");
+
+/**
+ * @param {{ body, params, query }} param0
+ * @param {{ body: Joi.Schema, params: Joi.Schema, query: Joi.Schema }} schema
+ * @returns
+ */
+
+const httpValidator = ({ body, params, query }, schema) => {
+};
+
+module.exports = httpValidator;
