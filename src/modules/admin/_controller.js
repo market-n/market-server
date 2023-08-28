eb20efe9b33afb181f7ca50d46b35fe598b0bc28
Baxtiyorov She'rozbek <91471554+sherozbek1706@users.noreply.github.com>
2023-08-28 12:29:14 +0500

    httpValidator({ body: req.body }, postLoginAdminSchemas);

--- src/modules/admin/_controller.js
+++ src/modules/admin/_controller.js
@@ -45,6 +45,7 @@ const add_admin = async (req, res, next) => {
 
 const login_admin = async (req, res, next) => {
   try {
+    httpValidator({ body: req.body }, postLoginAdminSchemas);
     let result = await loginAdmin({ body: req.body });
     res.status(200).json({ data: { token: result } });
   } catch (error) {
