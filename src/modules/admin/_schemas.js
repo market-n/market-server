5018284e819f85f910e342364a223f3a8e540290
Baxtiyorov She'rozbek <91471554+sherozbek1706@users.noreply.github.com>
2023-08-28 12:24:37 +0500

    postloginadminschemas yasb oldim

--- src/modules/admin/_schemas.js
+++ src/modules/admin/_schemas.js
@@ -33,3 +33,20 @@ const postAddAdminSchemas = {
   }),
 };
 
+const postLoginAdminSchemas = {
+  body: Joi.object({
+    username: Joi.string().min(6).max(64).required().messages({
+      "string.empty": "Username kiritilishi kerak!",
+      "string.min": "Username uzunligi 6 tadan ko'p bolishi kerak!",
+      "string.max": "Username uzunligi 64 tadan kam bolishi kerak!",
+      "any.required": "Username kiritilishi kerak!",
+    }),
+    password: Joi.string().min(6).max(64).required().messages({
+      "string.empty": "Password kiritilishi kerak!",
+      "string.min": "Password uzunligi 6 tadan ko'p bolishi kerak!",
+      "string.max": "Password uzunligi 64 tadan kam bolishi kerak!",
+      "any.required": "Password kiritilishi kerak!",
+    }),
+  }),
+};
+
