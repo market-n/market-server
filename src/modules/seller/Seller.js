5ac4e44c6230b197c578354db1a3db679d1bd665
Baxtiyorov She'rozbek <91471554+sherozbek1706@users.noreply.github.com>
2023-09-13 15:00:02 +0500

    Schema yasdim. Model yasab olaman.

--- src/modules/seller/Seller.js
+++ src/modules/seller/Seller.js
@@ -5,3 +5,52 @@ const type = {
   required: true,
 };
 
+const SellerSchema = new mongoose.Schema(
+  {
+    first_name: type,
+
+    last_name: type,
+
+    city: type,
+
+    email: {
+      ...type,
+      unique: true,
+    },
+
+    phone_number: {
+      ...type,
+      unique: true,
+    },
+
+    is_allow: {
+      type: mongoose.SchemaTypes.Boolean,
+      default: false,
+    },
+
+    admin_id: {
+      type: mongoose.SchemaTypes.ObjectId,
+      ref: "admin",
+      default: "",
+    },
+
+    username: {
+      ...type,
+      unique: true,
+    },
+
+    password: type,
+
+    is_deleted: {
+      type: mongoose.SchemaTypes.Boolean,
+      default: false,
+    },
+  },
+  {
+    versionKey: false,
+    timestamps: {
+      createdAt: "created_at",
+      updatedAt: "updated_at",
+    },
+  },
+);
