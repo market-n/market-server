86df3081d57142014d2e5119b49ccf8b16aa7dfd
Baxtiyorov She'rozbek <91471554+sherozbek1706@users.noreply.github.com>
2023-09-13 20:09:20 +0500

    controllerni apiga uladim.

--- src/modules/seller/_api.js
+++ src/modules/seller/_api.js
@@ -1,8 +1,21 @@
-const { registerSeller, loginSeller } = require("./_controller");
-
+const {
+  registerSeller,
+  loginSeller,
+  changeAllowSeller,
+} = require("./_controller");
+const { hasRole, isloggedIn } = require("../../shared/auth");
+const isMongoId = require("../../shared/validator/isMongoId");
+const isLoggedIn = require("../../shared/auth/isLoggedIn");
 const router = require("express").Router();
+
+const mChangeAllowSeller = [
+  isLoggedIn,
+  hasRole(["super_admin", "admin"]),
+  isMongoId,
+];
 
 router.post("/register/seller", registerSeller);
 router.post("/login/seller", loginSeller);
+router.post("/change/allow/seller/:id", mChangeAllowSeller, changeAllowSeller);
 
 module.exports = router;
