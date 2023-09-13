e8828ee6dd43b02f25696ae4976334a18dd72f7c
Baxtiyorov She'rozbek <91471554+sherozbek1706@users.noreply.github.com>
2023-09-13 20:26:01 +0500

    controllerni apiga berib yubordim.

--- src/modules/seller/_api.js
+++ src/modules/seller/_api.js
@@ -2,6 +2,7 @@ const {
   registerSeller,
   loginSeller,
   changeAllowSeller,
+  removeSeller,
 } = require("./_controller");
 const { hasRole, isloggedIn } = require("../../shared/auth");
 const isMongoId = require("../../shared/validator/isMongoId");
@@ -14,8 +15,15 @@ const mChangeAllowSeller = [
   isMongoId,
 ];
 
+const mRemoveSeller = [
+  isLoggedIn,
+  hasRole(["super_admin", "admin"]),
+  isMongoId,
+];
+
 router.post("/register/seller", registerSeller);
 router.post("/login/seller", loginSeller);
 router.post("/change/allow/seller/:id", mChangeAllowSeller, changeAllowSeller);
+router.delete("/seller/:id", mRemoveSeller, removeSeller);
 
 module.exports = router;
