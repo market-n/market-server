5737d40fb6ff61b67f39bf3a4ad2e9b67f5e4149
Baxtiyorov She'rozbek <91471554+sherozbek1706@users.noreply.github.com>
2023-09-14 16:28:56 +0500

    Product uchun schema va model

---
+++ src/modules/product/Product.js
@@ -0,0 +1,55 @@
+const mongoose = require("mongoose");
+
+const type = {
+  type: mongoose.SchemaTypes.String,
+  required: true,
+};
+
+const ProductSchema = new mongoose.Schema(
+  {
+    name: type,
+    description: type,
+    image: type,
+    price: {
+      type: mongoose.SchemaTypes.Number,
+      required: true,
+    },
+    quantity: {
+      type: mongoose.SchemaTypes.Number,
+      required: true,
+    },
+    category_id: {
+      type: mongoose.SchemaTypes.ObjectId,
+      ref: "category",
+      default: null,
+    },
+    market_id: {
+      type: mongoose.SchemaTypes.ObjectId,
+      ref: "market",
+      required: true,
+    },
+    measurement: {
+      type: mongoose.SchemaTypes.String,
+      enum: ["kg", "dona", "pochka", "blok"],
+      required: true,
+    },
+    seller_id: {
+      type: mongoose.SchemaTypes.ObjectId,
+      ref: "seller",
+      required: true,
+    },
+    is_deleted: {
+      type: mongoose.SchemaTypes.Boolean,
+      default: false,
+    },
+  },
+  {
+    versionKey: false,
+    timestamps: {
+      createdAt: "created_at",
+      updatedAt: "updated_at",
+    },
+  },
+);
+const Product = mongoose.model("product", ProductSchema);
+module.exports = Product;
