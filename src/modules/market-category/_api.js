9769fc0478d7d21ed62efef6929999c8c35d0e74
Baxtiyorov She'rozbek <91471554+sherozbek1706@users.noreply.github.com>
2023-09-14 11:24:04 +0500

    controllerlarni malumotini routerga berib yubordim.

--- src/modules/market-category/_api.js
+++ src/modules/market-category/_api.js
@@ -1,11 +1,22 @@
 const { isloggedIn, hasRole } = require("../../shared/auth");
-const { AddMarketCategory, ListMarketCategory } = require("./_controller");
+const isMongoId = require("../../shared/validator/isMongoId");
+const {
+  AddMarketCategory,
+  ListMarketCategory,
+  EditMarketCategory,
+} = require("./_controller");
 
 const router = require("express").Router();
 
 const mAddCategoryMarket = [isloggedIn, hasRole(["super_admin", "admin"])];
+const mEditCategoryMarket = [
+  isloggedIn,
+  hasRole(["super_admin", "admin"]),
+  isMongoId,
+];
 
 router.post("/market-category", mAddCategoryMarket, AddMarketCategory);
 router.get("/market-category", ListMarketCategory);
+router.patch("/market-category/:id", mEditCategoryMarket, EditMarketCategory);
 
 module.exports = router;
