b4734b1fbbf5dc28dca6001300bb9c44f7c7afca
Baxtiyorov She'rozbek <91471554+sherozbek1706@users.noreply.github.com>
2023-09-14 16:37:36 +0500

    controllerlarni routerga uladim.

--- src/modules/product/_api.js
+++ src/modules/product/_api.js
@@ -1,11 +1,13 @@
 const router = require("express").Router();
 const { hasRole, isloggedIn } = require("../../shared/auth");
 const isMongoId = require("../../shared/validator/isMongoId");
-const { addProduct, listProduct } = require("./_controller");
+const { addProduct, listProduct, showProduct } = require("./_controller");
 
 const mAddProduct = [isloggedIn, hasRole(["seller"])];
+const mShowProduct = [isloggedIn, isMongoId];
 
 router.post("/product", mAddProduct, addProduct);
 router.get("/product", listProduct);
+router.get("/product/:id", mShowProduct, showProduct);
 
 module.exports = router;
