1fc43ee4c50068e00f303c5c34ae455b388d2383
Baxtiyorov She'rozbek <91471554+sherozbek1706@users.noreply.github.com>
2023-09-14 19:37:48 +0500

    Patch qilish uchun route uladim.

--- src/modules/product/_api.js
+++ src/modules/product/_api.js
@@ -7,6 +7,7 @@ const {
   showProduct,
   removeProduct,
   unremoveProduct,
+  editProduct,
 } = require("./_controller");
 
 const mAddProduct = [isloggedIn, hasRole(["seller"])];
@@ -23,10 +24,17 @@ const mUnRemoveProduct = [
   isMongoId,
 ];
 
+const mEditProduct = [
+  isloggedIn,
+  hasRole(["admin", "super_admin", "seller"]),
+  isMongoId,
+];
+
 router.post("/product", mAddProduct, addProduct);
 router.get("/product", listProduct);
 router.get("/product/:id", mShowProduct, showProduct);
 router.delete("/product/:id", mRemoveProduct, removeProduct);
 router.delete("/product/un/:id", mUnRemoveProduct, unremoveProduct);
+router.patch("/product/:id", mEditProduct, editProduct);
 
 module.exports = router;
