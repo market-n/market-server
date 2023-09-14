17e2bbc14d30b9807926e09cd072d38d0ba291b2
Baxtiyorov She'rozbek <91471554+sherozbek1706@users.noreply.github.com>
2023-09-14 10:52:59 +0500

    listmarketcategory controlleri routerga ulandi

--- src/modules/market-category/_api.js
+++ src/modules/market-category/_api.js
@@ -1,10 +1,11 @@
 const { isloggedIn, hasRole } = require("../../shared/auth");
-const { AddMarketCategory } = require("./_controller");
+const { AddMarketCategory, ListMarketCategory } = require("./_controller");
 
 const router = require("express").Router();
 
 const mAddCategoryMarket = [isloggedIn, hasRole(["super_admin", "admin"])];
 
 router.post("/market-category", mAddCategoryMarket, AddMarketCategory);
+router.get("/market-category", ListMarketCategory);
 
 module.exports = router;
