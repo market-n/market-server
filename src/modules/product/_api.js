7bd87461c1d6fa2780b3df9eeacb6ba1bc5a7bcb
Baxtiyorov She'rozbek <91471554+sherozbek1706@users.noreply.github.com>
2023-09-14 16:27:09 +0500

    routerni add qilib oldim.

---
+++ src/modules/product/_api.js
@@ -0,0 +1,10 @@
+const router = require("express").Router();
+const { hasRole, isloggedIn } = require("../../shared/auth");
+const isMongoId = require("../../shared/validator/isMongoId");
+const { addProduct, listProduct } = require("./_controller");
+
+const mAddProduct = [isloggedIn, hasRole(["seller"])];
+
+router.post("/product", mAddProduct, addProduct);
+
+module.exports = router;
