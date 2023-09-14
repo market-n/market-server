c6003188aafc5b9da71adf6789c20b2978f082d6
Baxtiyorov She'rozbek <91471554+sherozbek1706@users.noreply.github.com>
2023-09-14 17:19:00 +0500

    delete qilish uchun controllerni routerga berib yubordim.

--- src/modules/product/_api.js
+++ src/modules/product/_api.js
@@ -1,13 +1,25 @@
 const router = require("express").Router();
 const { hasRole, isloggedIn } = require("../../shared/auth");
 const isMongoId = require("../../shared/validator/isMongoId");
-const { addProduct, listProduct, showProduct } = require("./_controller");
+const {
+  addProduct,
+  listProduct,
+  showProduct,
+  removeProduct,
+} = require("./_controller");
 
 const mAddProduct = [isloggedIn, hasRole(["seller"])];
 const mShowProduct = [isloggedIn, isMongoId];
+const mRemoveProduct = [
+  isloggedIn,
+  hasRole(["admin", "super_admin", "seller"]),
+  isMongoId,
+];
+
 
 router.post("/product", mAddProduct, addProduct);
 router.get("/product", listProduct);
 router.get("/product/:id", mShowProduct, showProduct);
+router.delete("/product/:id", mRemoveProduct, removeProduct);
 
 module.exports = router;
