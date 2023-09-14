abaf89bf20f84766b634ffb91a5290ab95d6287b
Baxtiyorov She'rozbek <91471554+sherozbek1706@users.noreply.github.com>
2023-09-14 16:28:03 +0500

    category_id hisob ga olib, add qilishni ko'rib chiqdim.

---
+++ src/modules/product/add-product.js
@@ -0,0 +1,26 @@
+const Product = require("./Product");
+const Market = require("../market/Market");
+// const ProductCategory = require("../product-category/ProductCategory");
+const { NotFoundError } = require("../../shared/errors");
+
+const addProductServices = async ({ body, user }) => {
+  // const { market_id, category_id, ...data } = body;
+  const { market_id, ...data } = body;
+
+  const existingMarket = await Market.findOne({ _id: market_id });
+
+  if (!existingMarket) {
+    throw new NotFoundError("Market Not Found!");
+  }
+
+  // const existingProductCategory = await ProductCategory.findOne({ _id: category_id });
+
+  // if (!existingProductCategory) {
+  //   throw new NotFoundError("Product Category Not Found!");
+  // }
+
+  // return Product.create({ market_id, category_id, ...data });
+  return Product.create({ market_id, ...data, seller_id: user.id });
+};
+
+module.exports = addProductServices;
