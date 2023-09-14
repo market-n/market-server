7078c6b1cdd7d634596e5eca2f4b58253c07563e
Baxtiyorov She'rozbek <91471554+sherozbek1706@users.noreply.github.com>
2023-09-14 16:38:01 +0500

    show Product uchun controller yasb oldim.

--- src/modules/product/_controller.js
+++ src/modules/product/_controller.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const addProductServices = require("./add-product");
 const listProductServices = require("./list-product");
+const showProductServices = require("./show-product");
 
 /**
  *
@@ -32,7 +33,22 @@ const listProduct = async (req, res, next) => {
   }
 };
 
+/**
+ *
+ * @param {express.Request} req
+ * @param {express.Response} res
+ * @param {express.NextFunction} next
+ */
+const showProduct = async (req, res, next) => {
+  try {
+    const result = await showProductServices({ params: req.params });
+    res.status(200).json({ data: result });
+  } catch (error) {
+    next(error);
+  }
+};
 module.exports = {
   addProduct,
   listProduct,
+  showProduct,
 };
