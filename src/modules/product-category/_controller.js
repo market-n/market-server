b6a385fcb9bca8396347168ee31621701dab44bb
Baxtiyorov She'rozbek <91471554+sherozbek1706@users.noreply.github.com>
2023-09-15 15:40:41 +0500

    product category uchun controller yasab oldim.

--- src/modules/product-category/_controller.js
+++ src/modules/product-category/_controller.js
@@ -1,1 +1,22 @@
+const express = require("express");
+const AddproductCategoryServices = require("./add-product-category");
+
+/**
+ *
+ * @param {express.Request} req
+ * @param {express.Response} res
+ * @param {express.NextFunction} next
+ */
+
+const AddproductCategory = async (req, res, next) => {
+  try {
+    const result = await AddproductCategoryServices({
+      body: req.body,
+      user: req.user,
+    });
+    return res.status(201).json({ data: result });
+  } catch (error) {
+    next(error);
+  }
+};
 
