324dd7853def4fa75ed60a4c1d4fe9f1630e1807
Baxtiyorov She'rozbek <91471554+sherozbek1706@users.noreply.github.com>
2023-09-19 22:50:17 +0500

    Agar username berilanidek qidirilganida topilmasa xato berilsin.

--- src/modules/user/login-user.js
+++ src/modules/user/login-user.js
@@ -1,5 +1,11 @@
 const loginUserService = async ({ body }) => {
   const { username, password } = body;
 
+  const existing = await User.findOne({ username, is_deleted: false });
+
+  if (!existing) {
+    throw new NotFoundError("User Not Found");
+  }
+
 };
 module.exports = loginUserService;
