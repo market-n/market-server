fc913491861f968ef8087fb1b1c98473d3ec6d64
Baxtiyorov She'rozbek <91471554+sherozbek1706@users.noreply.github.com>
2023-09-19 22:31:21 +0500

    Agar telefon oldin ishlatilgan bo'lsa xato berishi kerak.

--- src/modules/user/register-user.js
+++ src/modules/user/register-user.js
@@ -1,4 +1,5 @@
 const User = require("./User");
+const { BadRequestError } = require("../../shared/errors");
 const registerUserServices = async ({ body }) => {
   const { email, phone_number, username, password, ...data } = body;
   let existing = await User.findOne({ email });
@@ -6,6 +7,13 @@ const registerUserServices = async ({ body }) => {
   // Agar Email Topilsa Xato
   if (existing) {
     throw new BadRequestError("This email already existed!");
+  }
+
+  existing = await User.findOne({ phone_number });
+
+  // Agar Telefon Raqam Topilsa Xato
+  if (existing) {
+    throw new BadRequestError("This phone number already existed!");
   }
 
   const user = await User.create(userModel);
