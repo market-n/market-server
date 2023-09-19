75cc55a1105993d289c054525675b2687c84d5a1
Baxtiyorov She'rozbek <91471554+sherozbek1706@users.noreply.github.com>
2023-09-19 22:31:46 +0500

    Parolni hashlab va usernamelarni to'g'irlash kerak!

--- src/modules/user/register-user.js
+++ src/modules/user/register-user.js
@@ -1,5 +1,6 @@
 const User = require("./User");
 const { BadRequestError } = require("../../shared/errors");
+const { hash } = require("bcryptjs");
 const registerUserServices = async ({ body }) => {
   const { email, phone_number, username, password, ...data } = body;
   let existing = await User.findOne({ email });
@@ -15,6 +16,20 @@ const registerUserServices = async ({ body }) => {
   if (existing) {
     throw new BadRequestError("This phone number already existed!");
   }
+
+  existing = await User.findOne({ username });
+
+  // Agar Username Topilsa Xato
+  if (existing) {
+    throw new BadRequestError("This username already existed!");
+  }
+
+  const hashedPassword = await hash(password, 10);
+
+  const userModel = {
+    ...body,
+    password: hashedPassword,
+  };
 
   const user = await User.create(userModel);
   return user;
