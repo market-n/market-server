341c4acf789d7eceb1b9f90a659d5cfdff90c114
Baxtiyorov She'rozbek <91471554+sherozbek1706@users.noreply.github.com>
2023-09-19 22:21:54 +0500

    user register uchun controller yasab olindi

--- src/modules/user/_controller.js
+++ src/modules/user/_controller.js
@@ -1,6 +1,19 @@
 const express = require("express");
 const registerUserServices = require("./add-user");
 
+/**
+ *
+ * @param {express.Request} req
+ * @param {express.Response} res
+ * @param {express.NextFunction} next
+ */
+const registerUser = async (req, res, next) => {
+  try {
+  } catch (error) {
+    next(error);
+  }
+};
+
 module.exports = {
   registerUser,
 };
