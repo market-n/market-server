1833cf24e10acf85d2ce087aca3890425ee2ffa3
Baxtiyorov She'rozbek <91471554+sherozbek1706@users.noreply.github.com>
2023-09-21 16:00:05 +0500

    userni controllerini routerga ulandi

--- src/modules/user/_api.js
+++ src/modules/user/_api.js
@@ -1,9 +1,20 @@
-const { registerUser, loginUser, listUser } = require("./_controller");
+const { hasRole, isloggedIn } = require("../../shared/auth");
+const isMongoId = require("../../shared/validator/isMongoId");
+
+const {
+  registerUser,
+  loginUser,
+  listUser,
+  showUser,
+} = require("./_controller");
 
 const router = require("express").Router();
+
+const mShowUser = [isloggedIn, isMongoId];
 
 router.post("/user/register", registerUser);
 router.post("/user/login", loginUser);
 router.get("/user", listUser);
+router.get("/user/:id", mShowUser, showUser);
 
 module.exports = router;
