2722382ca30dadd5a77c0cbed1b4ea47fd5297d1
Baxtiyorov She'rozbek <91471554+sherozbek1706@users.noreply.github.com>
2023-09-21 16:08:15 +0500

    agar user topilmasa error qaytsin.

--- src/modules/user/show-user.js
+++ src/modules/user/show-user.js
@@ -3,7 +3,11 @@ const User = require("./User");
 const { NotFoundError } = require("../../shared/errors");
 
 const showUserServices = async ({ params }) => {
+  const existing = await User.findOne({ _id: params.id, is_deleted: false });
 
+  if (!existing) {
+    throw new NotFoundError("User Not Found!");
+  }
 
   return existing;
 };
