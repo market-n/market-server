b6c2bfe167df20f95007429fb44848ab0617c8ff
Baxtiyorov She'rozbek <91471554+sherozbek1706@users.noreply.github.com>
2023-09-21 16:01:45 +0500

    userni alohida ko'rsatish uchun services

--- src/modules/user/show-user.js
+++ src/modules/user/show-user.js
@@ -1,1 +1,10 @@
+const User = require("./User");
 
+const { NotFoundError } = require("../../shared/errors");
+
+const showUserServices = async ({ params }) => {
+
+
+  return existing;
+};
+module.exports = showUserServices;
