2334b9379f0fcd02e3bfdb95432ad92e1d341cd7
Baxtiyorov She'rozbek <91471554+sherozbek1706@users.noreply.github.com>
2023-09-25 23:26:23 +0500

    Update _api.js

--- src/modules/market-comment/_api.js
+++ src/modules/market-comment/_api.js
@@ -1,8 +1,12 @@
-const { addMarketComment } = require("./_controller");
+const { addMarketComment, listMarketComment } = require("./_controller");
 const { isloggedIn, hasRole } = require("../../shared/auth");
+const MongoId = require("../../shared/validator/isMongoId");
 const router = require("express").Router();
 
 const mAddMarketComment = [isloggedIn];
+const mListMarketComment = [MongoId];
+
 router.post("/market-comment", mAddMarketComment, addMarketComment);
+router.get("/market-comment/:id", mListMarketComment, listMarketComment);
 
 module.exports = router;
