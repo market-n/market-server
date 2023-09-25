d19f8c12d466094623bc257bd9bc0b79ec328aef
Baxtiyorov She'rozbek <91471554+sherozbek1706@users.noreply.github.com>
2023-09-25 19:57:07 +0500

    marketga mos commentni ko'rstatish uchun services

--- src/modules/market-comment/list-market-comment.js
+++ src/modules/market-comment/list-market-comment.js
@@ -1,1 +1,10 @@
+const MarketComment = require("./MarketComment");
+const Market = require("../market/Market");
+const { NotFoundError } = require("../../shared/errors");
 
+const listMarketCommentServices = async ({ params }) => {
+
+  return MarketComment.find({ market_id: params.id });
+};
+
+module.exports = listMarketCommentServices;
