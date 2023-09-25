97057d41683b099662419b1e56855ae83d1e55d4
Baxtiyorov She'rozbek <91471554+sherozbek1706@users.noreply.github.com>
2023-09-25 19:31:10 +0500

    Create _controller.js

---
+++ src/modules/market-comment/_controller.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const addMarketCommentServices = require("./add-comment");
+
+/**
+ *
+ * @param {express.Request} req
+ * @param {express.Response} res
+ * @param {express.NextFunction} next
+ */
+
+
