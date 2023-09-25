bf8c81eec63c5e51169cb67444800919bec8403f
Baxtiyorov She'rozbek <91471554+sherozbek1706@users.noreply.github.com>
2023-09-25 19:35:28 +0500

    agar market id topilmasa xato qaytarsin.

--- src/modules/market-comment/add-comment.js
+++ src/modules/market-comment/add-comment.js
@@ -5,6 +5,12 @@ const { NotFoundError } = require("../../shared/errors");
 const addMarketCommentServices = async ({ body, user }) => {
   const { market_id, comment } = body;
 
+  const existingMarket = await Market.findById({ _id: market_id });
+
+  if (!existingMarket) {
+    throw new NotFoundError("Market Not Found!");
+  }
+
   return MarketComment.create({ ...body, user_id: user.id });
 };
 
