17d463c264f317969d63389d87154e34a5c3750c
Baxtiyorov She'rozbek <91471554+sherozbek1706@users.noreply.github.com>
2023-09-25 19:35:10 +0500

    commenttariya qoldirish uchun service qismi

--- src/modules/market-comment/add-comment.js
+++ src/modules/market-comment/add-comment.js
@@ -1,1 +1,11 @@
+const MarketComment = require("./MarketComment");
+const Market = require("../market/Market");
+const { NotFoundError } = require("../../shared/errors");
 
+const addMarketCommentServices = async ({ body, user }) => {
+  const { market_id, comment } = body;
+
+  return MarketComment.create({ ...body, user_id: user.id });
+};
+
+module.exports = addMarketCommentServices;
