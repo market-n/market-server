c0bf7c1ab1a8e87d9a5815b061e39fadcf350bf6
Baxtiyorov She'rozbek <91471554+sherozbek1706@users.noreply.github.com>
2023-09-25 19:57:31 +0500

    Agar market berilgan id bo'yicha topilmasa errorni qaytarib yubor.

--- src/modules/market-comment/list-market-comment.js
+++ src/modules/market-comment/list-market-comment.js
@@ -3,6 +3,11 @@ const Market = require("../market/Market");
 const { NotFoundError } = require("../../shared/errors");
 
 const listMarketCommentServices = async ({ params }) => {
+  const existingMarket = await Market.findById({ _id: params.id });
+
+  if (!existingMarket) {
+    throw new NotFoundError("Market Not Found!");
+  }
 
   return MarketComment.find({ market_id: params.id });
 };
