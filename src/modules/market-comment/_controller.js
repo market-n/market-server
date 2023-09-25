741c27d72727eb109b5f30c33dbadbcf396595ed
Baxtiyorov She'rozbek <91471554+sherozbek1706@users.noreply.github.com>
2023-09-25 19:31:35 +0500

    market uchun commentariya yozish qismi controlleri

--- src/modules/market-comment/_controller.js
+++ src/modules/market-comment/_controller.js
@@ -8,4 +8,18 @@ const addMarketCommentServices = require("./add-comment");
  * @param {express.NextFunction} next
  */
 
+const addMarketComment = async (req, res, next) => {
+  try {
+    const result = await addMarketCommentServices({
+      body: req.body,
+      user: req.user,
+    });
+    res.status(201).json({ data: result });
+  } catch (error) {
+    next(error);
+  }
+};
 
+module.exports = {
+  addMarketComment,
+};
